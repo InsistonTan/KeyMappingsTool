0d84fa189ff84f2564231c609e6d631516e6f82d
m-RNA <m-RNA@qq.com>
2025-04-26 15:06:58 +0800

    [update] 添加 DirectInput 初始化和设备打开功能，更新UI 按键绑定向导逻辑

--- ets2keybinderwizard.cpp
+++ ets2keybinderwizard.cpp
@@ -1,13 +1,18 @@
 #include "ets2keybinderwizard.h"
+#include "BigKey.hpp"
 #include "global.h"
 #include "ui_ets2keybinderwizard.h"
 #include <QDateTime>
 #include <QDebug>
 #include <QDir>
 #include <QFileInfo>
+#include <QMessageBox>
 #include <map>
 #include <regex>
 #include <string>
+
+QString LOG_HEADER = "<b>特殊按键绑定</b> - ";
+QString MEG_BOX_LINE = "------------------------";
 
 // 参考开源项目：https://github.com/Sab1e-GitHub/ETS2-KeyBinder
 
@@ -16,17 +21,17 @@ using namespace std;
 // 枚举类型定义
 enum class BindingType
 {
-    lightoff,
-    lighthorn,
-    wipers0,
-    wipers1,
-    wipers2,
-    wipers3,
-    lightpark,
-    lighton,
-    hblight,
-    lblinkerh,
-    rblinkerh
+    lightoff,  // 关闭灯光
+    lighthorn, // 灯光喇叭
+    wipers0,   // 雨刷器关闭
+    wipers1,   // 雨刷器1档
+    wipers2,   // 雨刷器2档
+    wipers3,   // 雨刷器3档
+    lightpark, // 示廓灯
+    lighton,   // 近光灯
+    hblight,   // 远光灯
+    lblinkerh, // 左转向灯
+    rblinkerh  // 右转向灯
 };
 
 QString steamProfiles(QDir::homePath() + "/Documents/Euro Truck Simulator 2/steam_profiles");
@@ -49,7 +54,7 @@ ETS2KeyBinderWizard::ETS2KeyBinderWizard(QWidget* parent) : QWizard(parent), ui(
             ui->comboBox->addItem(item.name.data());
         }
     }
-    deviceNameInput = ui->comboBox->currentText().toStdString();
+    deviceName = ui->comboBox->currentText().toStdString();
     updateUserProfile(); // 更新用户配置文件
 
     connect(this, &QWizard::currentIdChanged, this, [=](int id) {
@@ -65,8 +70,8 @@ ETS2KeyBinderWizard::ETS2KeyBinderWizard(QWidget* parent) : QWizard(parent), ui(
                     ui->comboBox->addItem(item.name.data());
                 }
 
-                if (hasLastDevInCurrentDeviceList(deviceNameInput)) {
-                    ui->comboBox->setCurrentText(deviceNameInput.data());
+                if (hasLastDevInCurrentDeviceList(deviceName)) {
+                    ui->comboBox->setCurrentText(deviceName.data());
                 } else {
                     ui->comboBox->setCurrentIndex(-1);
                 }
@@ -74,11 +79,30 @@ ETS2KeyBinderWizard::ETS2KeyBinderWizard(QWidget* parent) : QWizard(parent), ui(
         } else if (id == 3) {
             // 备份配置文件
             backupProfile();
+            // 连接设备
+            if (deviceName.empty()) {
+                return;
+            }
+            if (!initDirectInput()) {
+                return;
+            }
+            if (!openDiDevice(ui->comboBox->currentIndex(), reinterpret_cast<HWND>(this->winId()))) {
+                return;
+            }
         }
     });
 }
 
 ETS2KeyBinderWizard::~ETS2KeyBinderWizard() {
+    if (pDevice) {
+        pDevice->Unacquire();
+        pDevice->Release();
+        pDevice = NULL;
+    }
+    if (pDirectInput) {
+        pDirectInput->Release();
+        pDirectInput = NULL;
+    }
     delete ui;
 }
 
@@ -119,7 +143,7 @@ bool ETS2KeyBinderWizard::backupProfile() {
 }
 
 void ETS2KeyBinderWizard::on_comboBox_activated(int index) {
-    deviceNameInput = ui->comboBox->currentText().toStdString();
+    deviceName = ui->comboBox->currentText().toStdString();
 }
 
 // 修改 controls.sii 文件
@@ -240,14 +264,185 @@ void ETS2KeyBinderWizard::updateUserProfile() {
     selectedProfilePath = steamProfiles + steamProfileFolders[0].first; // 默认选择第一个配置文件
 }
 
+// 初始化 DirectInput
+bool ETS2KeyBinderWizard::initDirectInput() {
+    if (pDirectInput != nullptr) {
+        return true;
+    }
+
+    HINSTANCE hInstance = GetModuleHandle(NULL);
+    if (FAILED(DirectInput8Create(hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&pDirectInput, NULL))) {
+        pushToQueue(parseSuccessLog(LOG_HEADER + "DirectInput 初始化失败！"));
+        qDebug() << "DirectInput 初始化失败！";
+        QMessageBox::critical(nullptr, "错误", "初始化DirectInput: 初始化失败！");
+        return false;
+    }
+
+    pushToQueue(parseSuccessLog(LOG_HEADER + "DirectInput 初始化成功！"));
+
+    return true;
+}
+
+// 打开选择的设备
+bool ETS2KeyBinderWizard::openDiDevice(int deviceIndex, HWND hWnd) {
+    if (deviceIndex < 0 || deviceIndex >= diDeviceList.size()) {
+        return false;
+    }
+
+    // 相同设备, 无需重复打开
+    if (pDevice && lastDeviceIndex == deviceIndex) {
+        return true;
+    }
+
+    // 新设备
+    lastDeviceIndex = deviceIndex;
+    if (pDevice) {
+        pDevice->Unacquire();
+        pDevice->Release();
+        pDevice = nullptr;
+    }
+
+    // 创建设备实例
+    if (FAILED(pDirectInput->CreateDevice(diDeviceList[deviceIndex].guidInstance, &pDevice, NULL))) {
+        qDebug() << "设备创建失败！";
+        QMessageBox::critical(this, "错误", "初始化设备: 设备创建失败！");
+        return false;
+    }
+
+    if (FAILED(pDevice->SetDataFormat(&c_dfDIJoystick2))) {
+        qDebug() << "设置数据格式失败！";
+        QMessageBox::critical(this, "错误", "初始化设备: 设置数据格式失败！");
+        return false;
+    }
+
+    if (FAILED(pDevice->SetCooperativeLevel(hWnd, DISCL_EXCLUSIVE | DISCL_BACKGROUND))) {
+        qDebug() << "设置独占模式失败！";
+        QMessageBox::critical(this, "错误", "初始化设备: 设置独占模式失败！");
+        return false;
+    }
+
+    // 获取控制器能力
+    capabilities.dwSize = sizeof(DIDEVCAPS);
+    if (FAILED(pDevice->GetCapabilities(&capabilities))) {
+        qDebug() << "获取设备能力失败！";
+        QMessageBox::critical(this, "错误", "初始化设备: 获取设备能力失败！");
+        return false;
+    }
+    // 获取按钮数量
+    qDebug() << "按钮数量:" << capabilities.dwButtons;
+
+    return true;
+}
+
 // 1、示廓灯&近光灯
-void ETS2KeyBinderWizard::on_pushButton_clicked() {}
+void ETS2KeyBinderWizard::on_pushButton_clicked() {
+    int stepSum = 3; // 步骤总数
+    // 1、将拨杆拨到关闭位置
+    QMessageBox box(QMessageBox::Information, "示廓灯&近光灯：1/" + QString::number(stepSum), "请将拨杆拧到：\n" + MEG_BOX_LINE + "\n关闭灯光");
+    box.exec();
+    // 2、将拨杆拨到示廓灯位置
+    box.setWindowTitle("示廓灯&近光灯：2/" + QString::number(stepSum));
+    box.setText("请将拨杆拧到：\n" + MEG_BOX_LINE + "\n示廓灯");
+    box.exec();
+
+    // 3、将拨杆拨到近光灯位置
+    box.setWindowTitle("示廓灯&近光灯：3/" + QString::number(stepSum));
+    box.setText("请将拨杆拧到：\n" + MEG_BOX_LINE + "\n近光灯");
+    box.exec();
+    // 4、确定是否绑定
+    box.setWindowTitle("示廓灯&近光灯");
+    box.setText("是否绑定？");
+    box.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    box.setDefaultButton(QMessageBox::Yes);
+
+    int ret = box.exec();
+    if (ret == QMessageBox::Yes) {
+        // 绑定操作
+        qDebug() << "绑定操作";
+    } else {
+        // 取消绑定操作
+        qDebug() << "取消绑定操作";
+    }
+}
 
 // 2、远光灯&灯光喇叭
-void ETS2KeyBinderWizard::on_pushButton_2_clicked() {}
+void ETS2KeyBinderWizard::on_pushButton_2_clicked() {
+    int stepSum = 3; // 步骤总数
+    // 1、将拨杆拨到关闭位置
+    QMessageBox box(QMessageBox::Information, "远光灯&灯光喇叭：1/" + QString::number(stepSum), "请将拨杆拨到：\n" + MEG_BOX_LINE + "\n关闭位置");
+    box.exec();
+    // 2、将拨杆拨到远光灯位置
+    box.setWindowTitle("远光灯&灯光喇叭：2/" + QString::number(stepSum));
+    box.setText("请将拨杆拨到：\n" + MEG_BOX_LINE + "\n远光灯");
+    box.exec();
+    // 3、将拨杆拨到灯光喇叭位置
+    box.setWindowTitle("远光灯&灯光喇叭：3/" + QString::number(stepSum));
+    box.setText("请将拨杆拨到：\n" + MEG_BOX_LINE + "\n灯光喇叭");
+    box.exec();
+    // 4、生成配置文件
+    box.setWindowTitle("远光灯&灯光喇叭");
+    box.setText("欧卡不支持远光灯的同步绑定，已生成配置文件，请回到主界面后用此配置文件开启全局映射。");
+    box.exec();
+}
 
 // 3、左转向灯&右转向灯
-void ETS2KeyBinderWizard::on_pushButton_3_clicked() {}
+void ETS2KeyBinderWizard::on_pushButton_3_clicked() {
+    int stepSum = 3; // 步骤总数
+    // 1、将拨杆拨到关闭位置
+    QMessageBox box(QMessageBox::Information, "左转向灯&右转向灯：1/" + QString::number(stepSum), "请将拨杆拨到：\n" + MEG_BOX_LINE + "\n关闭位置");
+    box.exec();
+    // 2、将拨杆拨到左转向灯位置
+    box.setWindowTitle("左转向灯&右转向灯：2/" + QString::number(stepSum));
+    box.setText("请将拨杆拨到：\n" + MEG_BOX_LINE + "\n左转向灯");
+    box.exec();
+    // 3、将拨杆拨到右转向灯位置
+    box.setWindowTitle("左转向灯&右转向灯：3/" + QString::number(stepSum));
+    box.setText("请将拨杆拨到：\n" + MEG_BOX_LINE + "\n右转向灯");
+    box.exec();
+    // 4、确定是否绑定
+    box.setWindowTitle("左转向灯&右转向灯");
+    box.setText("是否绑定？");
+    box.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    box.setDefaultButton(QMessageBox::Yes);
+    int ret = box.exec();
+    if (ret == QMessageBox::Yes) {
+        // 绑定操作
+        qDebug() << "绑定操作";
+    } else {
+        // 取消绑定操作
+        qDebug() << "取消绑定操作";
+    }
+}
 
 // 4、雨刮器
-void ETS2KeyBinderWizard::on_pushButton_4_clicked() {}
+void ETS2KeyBinderWizard::on_pushButton_4_clicked() {
+    int stepSum = 4; // 步骤总数
+    // 1、将拨杆拨到关闭位置
+    QMessageBox box(QMessageBox::Information, "雨刮器：1/" + QString::number(stepSum), "请将拨杆拨到：\n" + MEG_BOX_LINE + "\n关闭位置");
+    box.exec();
+    // 2、将拨杆拨到雨刮器1档位置
+    box.setWindowTitle("雨刮器：2/" + QString::number(stepSum));
+    box.setText("请将拨杆拨到\n" + MEG_BOX_LINE + "\n雨刮器1档");
+    box.exec();
+    // 3、将拨杆拨到雨刮器2档位置
+    box.setWindowTitle("雨刮器：3/" + QString::number(stepSum));
+    box.setText("请将拨杆拨到\n" + MEG_BOX_LINE + "\n雨刮器2档");
+    box.exec();
+    // 4、将拨杆拨到雨刮器3档位置
+    box.setWindowTitle("雨刮器：4/" + QString::number(stepSum));
+    box.setText("请将拨杆拨到\n" + MEG_BOX_LINE + "\n雨刮器3档");
+    box.exec();
+    // 5、确定是否绑定
+    box.setWindowTitle("雨刮器 绑定");
+    box.setText("是否绑定？");
+    box.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    box.setDefaultButton(QMessageBox::Yes);
+    int ret = box.exec();
+    if (ret == QMessageBox::Yes) {
+        // 绑定操作
+        qDebug() << "绑定操作";
+    } else {
+        // 取消绑定操作
+        qDebug() << "取消绑定操作";
+    }
+}
--- ets2keybinderwizard.h
+++ ets2keybinderwizard.h
@@ -1,10 +1,10 @@
 #ifndef ETS2KEYBINDERWIZARD_H
 #define ETS2KEYBINDERWIZARD_H
 
-#include "BigKey.hpp"
 #include <QWizard>
+#include <dinput.h>
 #include <string.h>
-
+#include <windows.h>
 
 namespace Ui {
 class ETS2KeyBinderWizard;
@@ -34,14 +34,22 @@ private slots:
 private:
     Ui::ETS2KeyBinderWizard* ui;
 
-    std::string deviceNameInput; // 设备名称
+    std::string deviceName; // 设备名称
 
     QString selectedProfilePath; // 选择的配置文件路径
 
     QList<QPair<QString, QDateTime>> steamProfileFolders;
     QList<QPair<QString, QDateTime>> profileFolders; // 所有配置文件列表
 
+    LPDIRECTINPUT8 pDirectInput = NULL;
+    LPDIRECTINPUTDEVICE8 pDevice = NULL;
+    int lastDeviceIndex = -99;
+    DIDEVCAPS capabilities;
+
     bool hasLastDevInCurrentDeviceList(std::string lastDeviceName);
+    bool openDiDevice(int deviceIndex, HWND hWnd);
+    bool initDirectInput();
+
     void updateUserProfile();
     bool backupProfile();
 };
