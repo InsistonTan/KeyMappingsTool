7f0dad9b84402371d419dd640834e17b24384194
InsistonTan <1763344790@qq.com>
2025-05-12 05:03:35 +0800

    [fix]修复轴映射键盘按键失败的bug

--- simulate_task.cpp
+++ simulate_task.cpp
@@ -402,16 +402,18 @@ QList<std::string> SimulateTask::getBtnStrListFromHandleMap(std::string btnStr){
 QList<MappingRelation*> SimulateTask::handleResult(QList<MappingRelation*> res){
     if(!res.isEmpty()){
         for(auto &currentBtn : res){
-            auto btnStr = currentBtn->dev_btn_name;
+            // 按键名称补上设备名称
+            auto btnStr = currentBtn->deviceName.toStdString() + "-" + currentBtn->dev_btn_name;
+
             // 轴映射键盘
-            if(!getIsXboxMode() && btnStr.find("轴") != std::string::npos){
+            if(!getIsXboxMode() && currentBtn->dev_btn_name.find("轴") != std::string::npos){
                 // 当前轴的值范围
                 auto currentRange = axisValueRangeMap.find(btnStr)->second;
                 int currentMin = currentRange.lMin, currentMax = currentRange.lMax;
                 int mid = (currentMax + currentMin)/2;
 
                 // 配置了盘面轴左转映射
-                if(handleMap.find(btnStr + "左转")!= handleMap.end()){
+                if(handleMap.find(btnStr + "左转") != handleMap.end()){
                     // 设置了轴反转
                     if(isAxisRotate(btnStr + "左转")){
                         if(currentBtn->dev_btn_value >= static_cast<int>(mid + ((currentMax - mid) * getInnerDeadAreaPanti()))){
@@ -428,7 +430,7 @@ QList<MappingRelation*> SimulateTask::handleResult(QList<MappingRelation*> res){
 
                 }
                 // 配置了盘面轴右转映射
-                if(handleMap.find(btnStr + "右转")!= handleMap.end()){
+                if(handleMap.find(btnStr + "右转") != handleMap.end()){
                     // 设置了轴反转
                     if(isAxisRotate(btnStr + "右转")){
                         if(currentBtn->dev_btn_value <= (mid - ((currentMax - mid) * getInnerDeadAreaPanti()))){
@@ -444,7 +446,7 @@ QList<MappingRelation*> SimulateTask::handleResult(QList<MappingRelation*> res){
 
                 }
                 // 配置了踏板轴映射
-                if(handleMap.find(btnStr)!= handleMap.end()){
+                if(handleMap.find(btnStr) != handleMap.end()){
                     // 设置了轴反转
                     if(isAxisRotate(btnStr)){
                         // 值小于内部死区范围不生效
@@ -520,7 +522,7 @@ void SimulateTask::doWork(){
 
             // 该按键存在映射, 模拟映射的键盘按键操作
             if (item != handleMap.end()) {
-                //qDebug("按键存在映射, 正在模拟对应操作");
+                //qDebug() << "按键[" << btnStr.data() << "]存在映射, 正在模拟对应操作";
 
                 // 按下了配置的暂停按键
                 if(item->second == PAUSE_BTN_VAL){
