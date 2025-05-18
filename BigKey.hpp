f3f9e6b08748eac75b1487b35c8633ed8efe0a05
m-RNA <m-RNA@qq.com>
2025-05-18 23:15:46 +0800

    [feature] 添加按键和摇杆的组合映射处理（摇杆角度为反码）& 添加`BigKey类`的 左右移、大于小于等于、uint构造函数

--- BigKey.hpp
+++ BigKey.hpp
@@ -1,5 +1,9 @@
 #pragma once
+#if defined(QT_VERSION)
 #define ENABLE_QT true
+#else
+#define ENABLE_QT false
+#endif
 
 #include <algorithm>
 #include <cmath>
@@ -31,12 +35,19 @@ class BigKey {
     const friend BigKey operator^(const BigKey&, const BigKey&);  // 按位异或重载
     const friend BigKey operator~(BigKey&);                       // 取反重载
 
+    const friend BigKey operator>>(const BigKey&, size_t);  // 右移重载
+    const friend BigKey operator<<(const BigKey&, size_t);  // 左移重载
+
     friend BigKey operator&=(BigKey&, const BigKey&);  // 按位与重载
     friend BigKey operator|=(BigKey&, const BigKey&);  // 按位或重载
 
     // 比较重载
     friend bool operator==(const BigKey&, const BigKey&);  // 等于重载
     friend bool operator!=(const BigKey&, const BigKey&);  // 不等于重载
+    friend bool operator<(const BigKey&, const BigKey&);   // 小于重载
+    friend bool operator<=(const BigKey&, const BigKey&);  // 小于等于重载
+    friend bool operator>(const BigKey&, const BigKey&);   // 大于重载
+    friend bool operator>=(const BigKey&, const BigKey&);  // 大于等于重载
 
     friend bool operator&&(const BigKey&, const BigKey&);  // 逻辑与重载
     friend bool operator&&(const BigKey&, const bool&);    // 逻辑与重载
@@ -52,6 +63,10 @@ class BigKey {
 public:
     BigKey();
     BigKey(const BigKey&);                //
+    BigKey(const uint8_t&);               // 用一个uint8_t构造
+    BigKey(const uint16_t&);              // 用一个uint16_t构造
+    BigKey(const uint32_t&);              // 用一个uint32_t构造
+    BigKey(const uint64_t&);              // 用一个uint64_t构造
     BigKey(const string&);                // 用一个字符串构造
     BigKey(BigKey&&) noexcept;            // 移动构造
     BigKey operator=(const BigKey&);      // 赋值函数
@@ -93,6 +108,26 @@ inline BigKey::BigKey(const BigKey& num) {
     }
 }
 
+inline BigKey::BigKey(const uint8_t& num) {
+    this->clear();  // 清空按键值
+    key[0] = num;
+}
+
+inline BigKey::BigKey(const uint16_t& num) {
+    this->clear();  // 清空按键值
+    key[0] = num;
+}
+
+inline BigKey::BigKey(const uint32_t& num) {
+    this->clear();  // 清空按键值
+    key[0] = num;
+}
+
+inline BigKey::BigKey(const uint64_t& num) {
+    this->clear();  // 清空按键值
+    key[0] = num;
+}
+
 // 移动构造
 inline BigKey::BigKey(BigKey&& num) noexcept {
     for (int i = 0; i < BIGKEY_NUMBER; i++) {
@@ -124,6 +159,76 @@ inline BigKey::operator bool() const {
     return false;  // 全为0
 }
 
+// 左移重载
+inline const BigKey operator<<(const BigKey& num1, size_t num2) {
+    if (num2 == 0) {
+        return num1;  // 如果左移的位数为0，直接返回原值
+    }
+
+    BigKey temp1(num1);
+    BigKey temp2(num1);
+
+    if (num2 >= sizeof(BIGKEY_TYPE_INTERNAL) * 8) {
+        // 如果左移的位数大于64位，则需要分段处理
+        size_t num = num2 / (sizeof(BIGKEY_TYPE_INTERNAL) * 8);
+        temp1.clear();
+        for (int i = BIGKEY_NUMBER - 1; i >= num; i--) {
+            temp1.key[i] = num1.key[i - num];  // 左移
+        }
+        num2 = num2 % (sizeof(BIGKEY_TYPE_INTERNAL) * 8);
+        temp2 = temp1;
+    }
+    if (num2 == 0) {
+        return temp1;  // 如果左移的位数为0，直接返回原值
+    }
+
+    temp1.key[0] = temp2.key[0] << num2;  // 左移
+    for (int i = 1; i < BIGKEY_NUMBER; i++) {
+        temp1.key[i] = (temp2.key[i] << num2);
+        {
+            BIGKEY_TYPE_INTERNAL temp3 = temp2.key[i - 1] >> (sizeof(BIGKEY_TYPE_INTERNAL) * 8 - num2);
+            temp1.key[i] |= temp3;
+        }
+    }
+
+    return temp1;
+}
+
+// 右移重载
+inline const BigKey operator>>(const BigKey& num1, size_t num2) {
+    if (num2 == 0) {
+        return num1;  // 如果右移的位数为0，直接返回原值
+    }
+
+    BigKey temp1(num1);
+    BigKey temp2(num1);
+
+    if (num2 >= sizeof(BIGKEY_TYPE_INTERNAL) * 8) {
+        // 如果右移的位数大于64位，则需要分段处理
+        size_t num = num2 / (sizeof(BIGKEY_TYPE_INTERNAL) * 8);
+        temp1.clear();
+        for (int i = 0; i < BIGKEY_NUMBER - num; i++) {
+            temp1.key[i] = num1.key[i + num];  // 右移
+        }
+        num2 = num2 % (sizeof(BIGKEY_TYPE_INTERNAL) * 8);
+        temp2 = temp1;
+    }
+    if (num2 == 0) {
+        return temp1;  // 如果右移的位数为0，直接返回原值
+    }
+
+    temp1.key[BIGKEY_NUMBER - 1] = temp2.key[BIGKEY_NUMBER - 1] >> num2;  // 右移
+    for (int i = BIGKEY_NUMBER - 2; i >= 0; i--) {
+        temp1.key[i] = (temp2.key[i] >> num2);
+        {
+            BIGKEY_TYPE_INTERNAL temp3 = temp2.key[i + 1] << (sizeof(BIGKEY_TYPE_INTERNAL) * 8 - num2);
+            temp1.key[i] |= temp3;
+        }
+    }
+
+    return temp1;
+}
+
 inline BigKey operator&=(BigKey& num1, const BigKey& num2) {
     for (int i = 0; i < BIGKEY_NUMBER; i++) {
         num1.key[i] &= num2.key[i];  // 按位与
@@ -138,9 +243,8 @@ inline BigKey operator|=(BigKey& num1, const BigKey& num2) {
     return num1;
 }
 
-inline const BigKey operator&(const BigKey& num1,
-                              const BigKey& num2)  // 按位与重载
-{
+// 按位与重载
+inline const BigKey operator&(const BigKey& num1, const BigKey& num2) {
     BigKey temp;
     for (int i = 0; i < BIGKEY_NUMBER; i++) {
         temp.key[i] = num1.key[i] & num2.key[i];  // 按位与
@@ -148,9 +252,8 @@ inline const BigKey operator&(const BigKey& num1,
     return temp;
 }
 
-inline const BigKey operator|(const BigKey& num1,
-                              const BigKey& num2)  // 按位或重载
-{
+// 按位或重载
+inline const BigKey operator|(const BigKey& num1, const BigKey& num2) {
     BigKey temp(num1);
     for (int i = 0; i < BIGKEY_NUMBER; i++) {
         temp.key[i] = num1.key[i] | num2.key[i];  // 按位或
@@ -158,8 +261,8 @@ inline const BigKey operator|(const BigKey& num1,
     return temp;
 }
 
-inline const BigKey operator^(const BigKey& num1, 
-                              const BigKey& num2) { // 按位异或重载
+inline const BigKey operator^(const BigKey& num1,
+                              const BigKey& num2) {  // 按位异或重载
     BigKey temp;
     for (int i = 0; i < BIGKEY_NUMBER; i++) {
         temp.key[i] = num1.key[i] ^ num2.key[i];  // 按位异或
@@ -245,8 +348,8 @@ inline string BigKey::toString() const {
     return str;
 }
 
-inline BigKey::BigKey(const string& num)  // 用字符串初始化
-{
+// 用字符串初始化
+inline BigKey::BigKey(const string& num) {
     if (num.size() > MAX_BUTTONS) {
         std::string errMsg = "String size exceeds maximum buttons " + std::to_string(num.size());
         throw std::out_of_range(errMsg);
@@ -259,31 +362,31 @@ inline BigKey::BigKey(const string& num)  // 用字符串初始化
     }
 }
 
-inline ostream& operator<<(ostream& out, const BigKey& num)  // 输出重载
-{
+// 输出重载
+inline ostream& operator<<(ostream& out, const BigKey& num) {
     out << num.toString();  // 打印时没有双引号
     return out;
 }
 
 #if ENABLE_QT
-inline QDebug operator<<(QDebug dbg, const BigKey& num)  // 输出重载 （注意，不是返回引用）
-{
+// 输出重载 （注意，不是返回引用）
+inline QDebug operator<<(QDebug dbg, const BigKey& num) {
     QString str = num.toString().data();  // 转换为QString
     dbg << qPrintable(str);               // 打印时没有双引号
     return dbg;
 }
 #endif
 
-inline istream& operator>>(istream& in, BigKey& num)  // 输入重载
-{
+// 输入重载
+inline istream& operator>>(istream& in, BigKey& num) {
     string str;
     in >> str;
     num = BigKey(str);
     return in;
 }
 
-inline bool operator==(const BigKey& num1, const BigKey& num2)  // 等于重载
-{
+// 等于重载
+inline bool operator==(const BigKey& num1, const BigKey& num2) {
     for (int i = 0; i < BIGKEY_NUMBER; i++) {
         if (num1.key[i] != num2.key[i]) {
             return false;  // 不相等
@@ -292,7 +395,34 @@ inline bool operator==(const BigKey& num1, const BigKey& num2)  // 等于重载
     return true;  // 相等
 }
 
-inline bool operator!=(const BigKey& num1, const BigKey& num2)  // 不等于重载
-{
+// 不等于重载
+inline bool operator!=(const BigKey& num1, const BigKey& num2) {
     return !(num1 == num2);  // 调用等于重载函数
+}
+
+// 小于重载
+inline bool operator<(const BigKey& num1, const BigKey& num2) {
+    for (int i = BIGKEY_NUMBER - 1; i >= 0; i--) {
+        if (num1.key[i] < num2.key[i]) {
+            return true;  // num1小于num2
+        } else if (num1.key[i] > num2.key[i]) {
+            return false;  // num1大于num2
+        }
+    }
+    return false;  // 相等
+}
+
+// 小于等于重载
+inline bool operator<=(const BigKey& num1, const BigKey& num2) {
+    return (num1 < num2 || num1 == num2);  // 调用小于和等于重载函数
+}
+
+// 大于重载
+inline bool operator>(const BigKey& num1, const BigKey& num2) {
+    return !(num1 <= num2);  // 调用小于等于重载函数
+}
+
+// 大于等于重载
+inline bool operator>=(const BigKey& num1, const BigKey& num2) {
+    return !(num1 < num2);  // 调用小于重载函数
 }
--- global.cpp
+++ global.cpp
@@ -378,7 +378,7 @@ QList<MappingRelation*> getInputState(bool enableLog, std::vector<MappingRelatio
             // 遍历按键，查看按键是否按下
             std::string btnStr = "";
             BUTTONS_VALUE_TYPE btnBitValue;
-            for (size_t i = 0; i < std::size(js.rgbButtons); i++) {
+            for (size_t i = 0; i < sizeof(js.rgbButtons); i++) {
                 if (js.rgbButtons[i] & 0x80) {
                     btnStr += "按键" + std::to_string(i) + "+";
                     btnBitValue.setBit(i, true);
@@ -395,36 +395,9 @@ QList<MappingRelation*> getInputState(bool enableLog, std::vector<MappingRelatio
 
                 }
             }
-
-            if (!btnStr.empty()) {
-                // 去掉最后的 "+"
-                btnStr = btnStr.substr(0, btnStr.length() - 1);
-
-                // multiBtnVector不为空, 需要进行多按键映射处理
-                if (multiBtnVector.size() > 0) {
-                    // 多按键映射，需要匹配按键，并拆分为多个 MappingRelation对象, 根据keyValue进行拆分
-                    for (auto multiBtn : multiBtnVector) {
-                        BUTTONS_VALUE_TYPE multiBtnBitValue = multiBtn.dev_btn_bit_value;
-                        if (multiBtn.deviceName == deviceName && (multiBtnBitValue) && ((multiBtnBitValue & btnBitValue) == multiBtnBitValue)) {
-                            // 找到对应的按键, 进行映射
-                            if (AssistFuncWindow::getEnableOnlyLongestMapping()) {
-                                btnBitValue &= (~multiBtnBitValue);  // 清除当前按键的值
-                            }
-                            MappingRelation *mapping = new MappingRelation(multiBtn.dev_btn_name, WHEEL_BUTTON, 0, 0, "", TriggerTypeEnum::Normal, deviceName);
-                            mapping->setBtnBitValue(multiBtnBitValue);  // 设置按键值
-                            list.append(mapping);
-                        }
-                    }
-                }else{
-                    MappingRelation *mapping =new MappingRelation(btnStr, WHEEL_BUTTON, 0, 0, "", TriggerTypeEnum::Normal, deviceName);
-                    mapping->setBtnBitValue(btnBitValue);  // 设置按键值
-                    list.append(mapping);
-                }
-            }
             if(enableLog && getEnableBtnLog()){
                 btnLog.append("}");
             }
-
 
             // 遍历摇杆数据
             // DWORD	rgdwPOV[4];
@@ -445,8 +418,10 @@ QList<MappingRelation*> getInputState(bool enableLog, std::vector<MappingRelatio
 
                         formatVal /= 10;
                     }
-                    std::string btnStr = "摇杆" + std::to_string(j+1) + "-角度" + std::to_string(formatVal);
-                    list.append(new MappingRelation(btnStr, WHEEL_BUTTON, val, 0, "", TriggerTypeEnum::Normal, deviceName));
+                    // 这里用反码操作, 因为摇杆的值是0-360度, 而没有操作时返回的也是0, 所以需要反码操作来表示操作
+                    BUTTONS_VALUE_TYPE povBitValue = (uint16_t)~formatVal;
+                    btnBitValue |= povBitValue << (j * 16 + sizeof(js.rgbButtons));
+                    btnStr += "摇杆" + std::to_string(j+1) + "-角度" + std::to_string(formatVal) + "+";
                 }
 
                 // 记录日志
@@ -463,6 +438,34 @@ QList<MappingRelation*> getInputState(bool enableLog, std::vector<MappingRelatio
                 povLog.append("}");
             }
 
+            if (!btnStr.empty()) {
+                // 去掉最后的 "+"
+                btnStr = btnStr.substr(0, btnStr.length() - 1);
+
+                // multiBtnVector不为空, 需要进行多按键映射处理
+                if (multiBtnVector.size() > 0) {
+                    // 多按键映射，需要匹配按键，并拆分为多个 MappingRelation对象, 根据keyValue进行拆分
+                    BUTTONS_VALUE_TYPE povClearFactor;
+                    povClearFactor = (BUTTONS_VALUE_TYPE)((uint64_t)-1) << 128; // 保留摇杆的值
+                    for (auto multiBtn : multiBtnVector) {
+                        BUTTONS_VALUE_TYPE multiBtnBitValue = multiBtn.dev_btn_bit_value;
+                        if (multiBtn.deviceName == deviceName && (multiBtnBitValue) && ((multiBtnBitValue & btnBitValue) == multiBtnBitValue) &&
+                            ((multiBtnBitValue & povClearFactor) == (btnBitValue & povClearFactor))) {
+                            // 找到对应的按键, 进行映射
+                            if (AssistFuncWindow::getEnableOnlyLongestMapping()) {
+                                btnBitValue &= (~multiBtnBitValue);  // 清除当前按键的值
+                            }
+                            MappingRelation *mapping = new MappingRelation(multiBtn.dev_btn_name, WHEEL_BUTTON, 0, 0, "", TriggerTypeEnum::Normal, deviceName);
+                            mapping->setBtnBitValue(multiBtnBitValue);  // 设置按键值
+                            list.append(mapping);
+                        }
+                    }
+                }else{
+                    MappingRelation *mapping =new MappingRelation(btnStr, WHEEL_BUTTON, 0, 0, "", TriggerTypeEnum::Normal, deviceName);
+                    mapping->setBtnBitValue(btnBitValue);  // 设置按键值
+                    list.append(mapping);
+                }
+            }
 
             // 映射xbox
             // if(getDefaultMappingType() == MappingType::Xbox){
@@ -660,7 +663,7 @@ bool hasXboxMappingInMappingList(std::vector<MappingRelation*> mappingList){
 // BUTTONS_VALUE_TYPE 转换为字符串
 std::string ButtonsValueTypeToString(BUTTONS_VALUE_TYPE btnValue){
     std::string btnValueStr = "";
-    for (size_t i = 0; i < MAX_BUTTONS; i++) {
+    for (size_t i = 0; i < 128; i++) {
         if (btnValue.getBit(i)) {
             btnValueStr += "按键" + std::to_string(i) + "+";
         }
