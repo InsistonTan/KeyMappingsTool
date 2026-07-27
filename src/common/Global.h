0ab2d47c45e96381baaed4432521246b90851536
InsistonTan <1763344790@qq.com>
2026-07-27 13:54:34 +0800

    [update]版本号更新至1.3.3

--- src/common/Global.h
+++ src/common/Global.h
@@ -46,7 +46,7 @@ public:
     // 旧版本的软件名称
     inline const static QString OLD_APP_NAME = "KeyMapping";
     // 当前软件版本号
-    inline const static QString CURRENT_VERSION = "1.3.2";
+    inline const static QString CURRENT_VERSION = "1.3.3";
 
     // 日志api
     inline const static QString LOGGER_API = "https://keymappingstoollogger.176334479.xyz";
